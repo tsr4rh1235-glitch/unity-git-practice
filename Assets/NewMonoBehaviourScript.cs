858b78c806e9c8ddb4be1785e581fa9c933dae06
ChangMin1 <tsr4rh1235@gmail.com>
2026-09-23 17:20:57 +0900

    3주차 수업
    거의 몰라서 거의 아무것도 못함

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -9,11 +9,21 @@ public class NewMonoBehaviourScript : MonoBehaviour
         a = 1;
         Debug.Log(a);
         // Debug.Log("hello World");
+
+        //bool a = true;
+        //bool b = false;
+        //gameObject.SetActive(a || b);
+        //Vector2 newPos = transform.position;
+        newPos.x = newPos.x + 5;
+        transform
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(0,1,0);
+        }
     }
 }
